40517b501ae9bd8d18e9d13999cd60a7ed37ae43
badmus <temitopebdms@gmail.com>
2020-12-20 14:45:06 +0100

    Latest commit

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Url(models.Model):
-    long_url = models.URLField("Original Url", max_length=200)
+    long_url = models.URLField("Original Url", max_length=5000)
     url_code = models.CharField("Url Code", max_length=200, null=True, blank=True)
     date_created = models.DateTimeField("Date Created", auto_now_add=True)
     
